948610f261a67e85caecd76ed4fc53ce54b86d55
Maman` <75215687+JfeStudio@users.noreply.github.com>
2023-12-01 00:37:40 +0700

    dummy data

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,3 +1,26 @@
+<script setup>
+import { portfolioPosts } from '../plugins/data.js'
+
+const colorTags = (tag) => {
+  switch (tag) {
+    case 'laravel':
+      return 'bg-red-100 text-red-500'
+    case 'tailwind':
+      return 'bg-sky-100 text-sky-500'
+    default:
+      return 'bg-indigo-100 text-indigo-500'
+  }
+}
+
+const colorStatus = (status) => {
+  switch (status) {
+    case 'private':
+      return 'bg-red-100 text-red-500'
+    default:
+      return 'bg-indigo-100 text-blue-500'
+  }
+}
+</script>
 <template>
   <!-- latest project -->
   <section id="project" class="-scroll-mt-12 py-16 lg:py-20">
@@ -6,192 +29,46 @@
     >
       My Latest Project
     </h1>
+    <!-- latest -->
     <div class="flex flex-wrap justify-center gap-7">
       <div
+        v-for="(post, index) in portfolioPosts"
+        :key="index"
         class="relative max-w-[18rem] overflow-hidden rounded-lg border border-slate-100 bg-white/70 p-1 text-center shadow-sm transition-transform duration-300 ease-in-out hover:-translate-y-1 2xl:max-w-sm"
       >
         <!-- badge for new project -->
         <div class="absolute right-3 top-2">
           <span
-            class="inline-flex scale-90 items-center rounded-full bg-indigo-100 px-2.5 py-0.5 text-xs font-medium text-blue-500"
-            >Public
+            :class="colorStatus(post.status)"
+            class="inline-flex scale-90 items-center rounded-full px-2.5 py-0.5 text-xs font-medium capitalize"
+          >
+            {{ post.status }}
           </span>
         </div>
-        <a href="https://github.com/arewtech/amerta" target="_blank">
+        <a v-if="post.link" :href="post.link" target="_blank">
           <div class="images-product overflow-hidden rounded-md border border-gray-50 shadow-3xl">
-            <img
-              class="cursor-pointer"
-              src="https://raw.githubusercontent.com/arewtech/amerta/main/public/design/preview-hero.png"
-              alt=""
-            />
+            <img :src="post.image" :alt="post.title" />
           </div>
         </a>
-        <div class="pt-4">
-          <p class="text-sm font-medium text-gray-500 lg:text-xs lg:text-gray-400">
-            Web Development
-          </p>
-          <h3 class="text-base font-semibold text-slate-900 lg:text-sm">Amerta</h3>
-        </div>
-        <div class="mb-4 mt-1 flex items-center justify-center">
-          <span
-            class="inline-flex scale-90 items-center rounded-sm bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-500"
-            >Laravel
-          </span>
-          <span
-            class="inline-flex scale-90 items-center rounded-sm bg-sky-100 px-2.5 py-0.5 text-xs font-medium text-sky-500"
-            >Tailwind
-          </span>
-        </div>
-      </div>
-      <div
-        class="relative max-w-[18rem] overflow-hidden rounded-lg border border-slate-100 bg-white/70 p-1 text-center shadow-sm transition-transform duration-300 ease-in-out hover:-translate-y-1 2xl:max-w-sm"
-      >
-        <!-- badge for new project -->
-        <div class="absolute right-3 top-2">
-          <span
-            class="inline-flex scale-90 items-center rounded-full bg-indigo-100 px-2.5 py-0.5 text-xs font-medium text-blue-500"
-            >Public
-          </span>
-        </div>
-        <a href="https://github.com/arewtech/luminary" target="_blank">
-          <div class="images-product overflow-hidden rounded-md border border-gray-50 shadow-3xl">
-            <img
-              class="cursor-pointer"
-              src="https://raw.githubusercontent.com/arewtech/luminary/development/public/assets/preview/dashboard.png"
-              alt="image"
-            />
-          </div>
-        </a>
-        <div class="pt-4">
-          <p class="text-sm font-medium text-gray-500 lg:text-xs lg:text-gray-400">
-            Web Development
-          </p>
-          <h3 class="text-base font-semibold text-slate-900 lg:text-sm">Luminary</h3>
-        </div>
-        <div class="mb-4 mt-1 flex items-center justify-center">
-          <span
-            class="inline-flex scale-90 items-center rounded-sm bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-500"
-            >Laravel
-          </span>
-          <span
-            class="inline-flex scale-90 items-center rounded-sm bg-indigo-100 px-2.5 py-0.5 text-xs font-medium text-indigo-500"
-            >Bootstrap
-          </span>
-        </div>
-      </div>
-      <div
-        class="relative max-w-[18rem] overflow-hidden rounded-lg border border-slate-100 bg-white/70 p-1 text-center shadow-sm transition-transform duration-300 ease-in-out hover:-translate-y-1 2xl:max-w-sm"
-      >
-        <!-- badge for new project -->
-        <div class="absolute right-3 top-2">
-          <span
-            class="inline-flex scale-90 items-center rounded-full bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-500"
-            >Private
-          </span>
-        </div>
-        <div class="images-product overflow-hidden rounded-md border border-gray-50 shadow-3xl">
-          <img class="cursor-pointer" src="/images/projects/fertilizers.png" alt="image" />
+        <div
+          v-else
+          class="images-product overflow-hidden rounded-md border border-gray-50 shadow-3xl"
+        >
+          <img :src="post.image" :alt="post.title" />
         </div>
         <div class="pt-4">
           <p class="text-sm font-medium text-gray-500 lg:text-xs lg:text-gray-400">
-            Web Development
+            {{ post.category }}
           </p>
-          <h3 class="text-base font-semibold text-slate-900 lg:text-sm">Fertilizers</h3>
+          <h3 class="text-base font-semibold text-slate-900 lg:text-sm">{{ post.title }}</h3>
         </div>
         <div class="mb-4 mt-1 flex items-center justify-center">
           <span
-            class="inline-flex scale-90 items-center rounded-sm bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-500"
-            >Laravel
-          </span>
-          <span
-            class="inline-flex scale-90 items-center rounded-sm bg-indigo-100 px-2.5 py-0.5 text-xs font-medium text-indigo-500"
-            >Bootstrap
-          </span>
-        </div>
-      </div>
-      <div
-        class="relative max-w-[18rem] overflow-hidden rounded-lg border border-slate-100 bg-white/70 p-1 text-center shadow-sm transition-transform duration-300 ease-in-out hover:-translate-y-1 2xl:max-w-sm"
-      >
-        <!-- badge for new project -->
-        <div class="absolute right-3 top-2">
-          <span
-            class="inline-flex scale-90 items-center rounded-full bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-500"
-            >Private
-          </span>
-        </div>
-        <div class="images-product overflow-hidden rounded-md border border-gray-50 shadow-3xl">
-          <img class="cursor-pointer" src="/images/projects/archives.png" alt="" />
-        </div>
-        <div class="pt-4">
-          <p class="text-sm font-medium text-gray-500 lg:text-xs lg:text-gray-400">
-            Web Development
-          </p>
-          <h3 class="text-base font-semibold text-slate-900 lg:text-sm">Archives</h3>
-        </div>
-        <div class="mb-4 mt-1 flex items-center justify-center">
-          <span
-            class="inline-flex scale-90 items-center rounded-sm bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-500"
-            >Laravel
-          </span>
-          <span
-            class="inline-flex scale-90 items-center rounded-sm bg-indigo-100 px-2.5 py-0.5 text-xs font-medium text-indigo-500"
-            >Bootstrap
-          </span>
-        </div>
-      </div>
-      <div
-        class="relative max-w-[18rem] overflow-hidden rounded-lg border border-slate-100 bg-white/70 p-1 text-center shadow-sm transition-transform duration-300 ease-in-out hover:-translate-y-1 2xl:max-w-sm"
-      >
-        <!-- badge for new project -->
-        <div class="absolute right-3 top-2">
-          <span
-            class="inline-flex scale-90 items-center rounded-full bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-500"
-            >Private
-          </span>
-        </div>
-        <div class="images-product overflow-hidden rounded-md border border-gray-50 shadow-3xl">
-          <img class="cursor-pointer" src="/images/projects/sekolahku.png" alt="" />
-        </div>
-        <div class="pt-4">
-          <p class="text-sm font-medium text-gray-500 lg:text-xs lg:text-gray-400">
-            Web Development
-          </p>
-          <h3 class="text-base font-semibold text-slate-900 lg:text-sm">Sekolahku</h3>
-        </div>
-        <div class="mb-4 mt-1 flex items-center justify-center">
-          <span
-            class="inline-flex scale-90 items-center rounded-sm bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-500"
-            >Laravel
-          </span>
-          <span
-            class="inline-flex scale-90 items-center rounded-sm bg-indigo-100 px-2.5 py-0.5 text-xs font-medium text-indigo-500"
-            >Bootstrap
-          </span>
-        </div>
-      </div>
-      <div
-        class="relative max-w-[18rem] overflow-hidden rounded-lg border border-slate-100 bg-white/70 p-1 text-center shadow-sm transition-transform duration-300 ease-in-out hover:-translate-y-1 2xl:max-w-sm"
-      >
-        <!-- badge for new project -->
-        <div class="absolute right-3 top-2">
-          <span
-            class="inline-flex scale-90 items-center rounded-full bg-indigo-100 px-2.5 py-0.5 text-xs font-medium text-blue-500"
-            >Public
-          </span>
-        </div>
-        <a href="https://iotm-co.github.io/slice-mentor/" target="_blank">
-          <div class="images-product overflow-hidden rounded-md border border-gray-50 shadow-3xl">
-            <img class="cursor-pointer" src="/images/projects/slicementor.png" alt="" />
-          </div>
-        </a>
-        <div class="pt-4">
-          <p class="text-sm font-medium text-gray-500 lg:text-xs lg:text-gray-400">Web Design</p>
-          <h3 class="text-base font-semibold text-slate-900 lg:text-sm">Slice Mentor</h3>
-        </div>
-        <div class="mb-4 mt-1 flex items-center justify-center">
-          <span
-            class="inline-flex scale-90 items-center rounded-sm bg-sky-100 px-2.5 py-0.5 text-xs font-medium text-sky-500"
-            >Tailwind
+            v-for="(tag, index) in post.tags"
+            :key="index"
+            :class="colorTags(tag)"
+            class="inline-flex scale-90 items-center rounded-sm px-2.5 py-0.5 text-xs font-medium capitalize"
+            >{{ tag }}
           </span>
         </div>
       </div>
---
+++ src/plugins/data.js
@@ -0,0 +1,51 @@
+export const portfolioPosts = [
+  {
+    title: 'Amerta',
+    category: 'Web Development',
+    image: 'https://raw.githubusercontent.com/arewtech/amerta/main/public/design/preview-hero.png',
+    link: 'https://github.com/arewtech/amerta',
+    status: 'public',
+    tags: ['laravel', 'tailwind']
+  },
+  {
+    title: 'Luminary',
+    category: 'Web Development',
+    image:
+      'https://raw.githubusercontent.com/arewtech/luminary/development/public/assets/preview/dashboard.png',
+    link: 'https://github.com/arewtech/luminary',
+    status: 'public',
+    tags: ['laravel', 'bootstrap']
+  },
+  {
+    title: 'Fertilizers',
+    category: 'Web Development',
+    image: '/images/projects/fertilizers.png',
+    link: '',
+    status: 'private',
+    tags: ['laravel', 'bootstrap']
+  },
+  {
+    title: 'Archives',
+    category: 'Web Development',
+    image: '/images/projects/archives.png',
+    link: '',
+    status: 'private',
+    tags: ['laravel', 'bootstrap']
+  },
+  {
+    title: 'Sekolahku',
+    category: 'Web Development',
+    image: '/images/projects/sekolahku.png',
+    link: '',
+    status: 'private',
+    tags: ['laravel', 'bootstrap']
+  },
+  {
+    title: 'Slice Mentor',
+    category: 'Web Design',
+    image: '/images/projects/slicementor.png',
+    link: 'https://iotm-co.github.io/slice-mentor/',
+    status: 'public',
+    tags: ['tailwind']
+  }
+]
